ffcefff01eb3f9f8526b66fb183854b84051d845
04abhisek2006 <abhisekpanda366@gmail.com>
2025-08-08 16:42:27 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "//Abhisek-Arena-2.OReact",
+  base: "/Abhisek-Arena-2.OReact/",
   plugins: [react()],
 })
